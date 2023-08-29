fef9e6738981f0a2d0ed67bd72d9f34d249e4617
ngda <ngdaaz@gmail.com>
2023-08-29 11:20:12 +0700

    update config

--- Neovim-from-scratch-MACOS/lua/user/colorscheme.lua
+++ Neovim-from-scratch-MACOS/lua/user/colorscheme.lua
@@ -1,4 +1,4 @@
-local colorscheme = "tokyonight"
+local colorscheme = "github_dark_dimmed"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
--- Neovim-from-scratch-MACOS/lua/user/treesitter.lua
+++ Neovim-from-scratch-MACOS/lua/user/treesitter.lua
@@ -1,10 +1,11 @@
-local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-if not status_ok then
-	return
-end
+local M = {
+}
+function M.config()
+  local treesitter = require "nvim-treesitter"
+  local configs = require "nvim-treesitter.configs"
 
-configs.setup({
-	ensure_installed = {
+  configs.setup {
+    ensure_installed = { 
 		"bash",
 		"c",
 		"javascript",
@@ -18,15 +19,26 @@ configs.setup({
 		"java",
 		"yaml",
 		"markdown",
-		"markdown_inline",
-	}, -- one of "all" or a list of languages
-	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
-	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
-	},
-	autopairs = {
-		enable = true,
-	},
-	indent = { enable = true, disable = { "python", "css" } },
-})
+		"markdown_inline"
+	}, -- put the language you want in this array
+    -- ensure_installed = "all", -- one of "all" or a list of languages
+    ignore_install = { "" },                                                       -- List of parsers to ignore installing
+    sync_install = false,                                                          -- install languages synchronously (only applied to `ensure_installed`)
+
+    highlight = {
+      enable = true,       -- false will disable the whole extension
+      disable = { "css" }, -- list of language that will be disabled
+    },
+    autopairs = {
+      enable = true,
+    },
+    indent = { enable = true, disable = { "python", "css" } },
+
+    context_commentstring = {
+      enable = true,
+      enable_autocmd = false,
+    },
+  }
+end
+
+return M
