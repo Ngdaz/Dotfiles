f9018a8ef885def0b8cb3acdd1403060fef39a80
Duy Anh Nguyen <duyanhnguyen@Mac-Pro.local>
2024-06-02 23:58:03 +0700

    add vue config

--- MyLazyVimConfig/lua/plugins/lsp-keymap.lua
+++ MyLazyVimConfig/lua/plugins/lsp-keymap.lua
@@ -1,6 +1,26 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    opts = {
+      setup = {
+        tsserver = function(_, opts)
+          local mason_registry = require("mason-registry")
+          local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+            .. "/node_modules/@vue/language-server"
+
+          opts.init_options = {
+            plugins = {
+              {
+                name = "@vue/typescript-plugin",
+                location = vue_language_server_path,
+                languages = { "vue" },
+              },
+            },
+          }
+          opts.filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+        end,
+      },
+    },
     init = function()
       local Util = require("lazyvim.util")
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
